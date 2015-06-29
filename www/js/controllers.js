0cfb14437b994e8c8add9718a22c137cafa5bdd8
thierryderuyttere <thierryderuyttere@hotmail.com>
2015-06-29 21:46:47 +0200

    Trying to add Qr-reader

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,6 +41,21 @@ angular.module('starter.controllers', [])
   };
 })
 
+.controller("scannerCtrl", function($scope, $cordovaBarcodeScanner) {
+
+  $scope.scanBarcode = function() {
+    $cordovaBarcodeScanner.scan().then(function(imageData) {
+      alert(imageData.text);
+      console.log("Barcode Format -> " + imageData.format);
+      console.log("Cancelled -> " + imageData.cancelled);
+    }, function(error) {
+      console.log("An error happened -> " + error);
+    });
+  };
+
+})
+
+
 .controller('scannerCtrl', function($scope){
 
     });
