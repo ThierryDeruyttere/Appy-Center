8a76b840afa97a17ebbe66f57f49451f52df99b3
thierryderuyttere <thierryderuyttere@hotmail.com>
2015-08-25 00:02:23 +0200

    Working file transfer

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,57 +1,78 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+        // Form data for the login modal
+        $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function() {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        // Open the login modal
+        $scope.login = function() {
+            $scope.modal.show();
+        };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function() {
+            console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function() {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
 
-.controller("scannerCtrl", function($scope, $cordovaBarcodeScanner) {
+    .controller("scannerCtrl", function($scope, $cordovaBarcodeScanner, $timeout, $cordovaFileTransfer) {
 
-  $scope.scanBarcode = function() {
-    console.log("Starting scan...");
-    $cordovaBarcodeScanner.scan().then(function(imageData) {
-      alert(imageData.text);
-      console.log("Barcode Format -> " + imageData.format);
-      console.log("Cancelled -> " + imageData.cancelled);
-    }, function(error) {
-      console.log("An error happened -> " + error);
+        $scope.scanBarcode = function() {
+            console.log("Starting scan...");
+            $cordovaBarcodeScanner.scan().then(function(imageData) {
+
+                var url = imageData.text;
+                var targetPath = cordova.file.documentsDirectory + "testImage.png";
+                var trustHosts = true;
+                var options = {};
+
+                $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+                    .then(function(result) {
+                        // Success!
+                        alert("Done!");
+                    }, function(err) {
+                        // Error
+                        alert("Error");
+                    }, function (progress) {
+                        $timeout(function () {
+                            $scope.downloadProgress = (progress.loaded / progress.total) * 100;
+                        })
+                    });
+            }, function(error) {
+                console.log("An error happened -> " + error);
+            });
+        };
+
+    })
+
+    .controller("storeCtrl", function($scope) {
+        $scope.download = function(){
+            alert("ok");
+        }
     });
-  };
-
-})
